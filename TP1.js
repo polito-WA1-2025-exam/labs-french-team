b5f8d16dcc44bc48caedbb8a4d9a514bae6d18ca
BenjaminTaranto <151042626+BenjaminTaranto@users.noreply.github.com>
2025-03-14 11:30:51 +0100

    Add files via upload

--- TP1.js
+++ TP1.js
@@ -13,12 +13,39 @@ function State(Grid,Difficulty){
     this.Limit=4*this.Difficulty;
     this.History=[];
 }
+
+function Collection(){
+    this.item=[];
+}
+Collection.prototype.add=function(object){
+    this.item.push(object);
+};
+Collection.prototype.GetProperty=function(object,value){
+    return this.item.filter(item => item[object]===value);
+};
+Collection.prototype.remove=function(object,value){
+    this.item=this.item.filter(item=> item[object]!==value)
+};
+Collection.prototype.Start=function(Size,object){
+    for (let i of Size){
+        Collection.push(object);
+    } 
+}
 function Game(){
     this.Score = 0;
-
     console.log("Let's the game begin ! Your score is",this.Score);
+    if (cpt>State.Limit) {
+        console.log("You lost the Game.");
+        let answer = prompt("Would try again ?");
+        if (answer==="Yes"){
+            let Difficulty = prompt("Which difficulty ?")
+            Start
+        }
+    }
+    }
     
 }
+
 let Trump= new Politician("Donald Trump","D","M","US")
 console.log(Trump)
 let game= new Game()
